8f9a59b13335d4c540d7b8ac8218cf88ee3d057b
Yagmur Tasci <yagmur_tascii@hotmail.com>
2024-08-14 17:37:01 +0300

    Apache Kafka Producer added.

---
+++ ApacheKafkaProducer/ApacheKafkaProducer/Program.cs
@@ -0,0 +1,17 @@
+﻿using Confluent.Kafka;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var config = new ProducerConfig
+        {
+            BootstrapServers = "localhost:29092" 
+        };
+        using (var producer = new ProducerBuilder<int, string>(config).Build())
+        {
+            await producer.ProduceAsync("test-topic", new Message<int, string> {Key = 0,Value = "Apache Kafka" });
+            producer.Flush(TimeSpan.FromSeconds(10));
+        }
+    }
+}
